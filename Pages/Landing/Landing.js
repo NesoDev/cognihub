6d11660661cfebb706381f6a17f32285010d45c4
magnanimous-se7en <jl.parrales.torres@gmail.com>
2024-10-10 05:27:03 -0500

    Carga de landing page

---
+++ Pages/Landing/Landing.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    alert('Iniciando sesión de lectura...');
+    // Aquí puedes agregar más lógica para lo que sucede al iniciar
+});
